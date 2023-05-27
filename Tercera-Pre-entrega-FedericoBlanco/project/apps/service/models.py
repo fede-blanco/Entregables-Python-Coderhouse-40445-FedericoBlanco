4cff8b6547e5acdfe4a66ecfcc6ba00f00e63529
fede-blanco <blancofedem@gmail.com>
2023-05-27 14:32:13 -0300

    Se agrega primer commit de la 3ra pre entrega

---
+++ Tercera-Pre-entrega-FedericoBlanco/project/apps/service/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    imagePath = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
